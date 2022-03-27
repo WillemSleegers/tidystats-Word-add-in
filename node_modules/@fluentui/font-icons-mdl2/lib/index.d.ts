9621206fe40d10170f2806e9b497d378457bceab
Willem Sleegers <w.sleegers@me.com>
2022-03-27 19:16:05 +0200

    Delete node_modules directory

--- node_modules/@fluentui/font-icons-mdl2/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { IIconOptions } from '@fluentui/style-utilities';
-export declare function initializeIcons(baseUrl?: string, options?: IIconOptions): void;
-export { IconNames } from './IconNames';
-import './version';
--- node_modules/office-addin-cli/README.md
+++
@@ -1,4 +0,0 @@
-# Office-Addin-CLI
-
-A command-line interface for Office Add-ins.
-
