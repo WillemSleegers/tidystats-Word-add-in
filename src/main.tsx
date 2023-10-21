23a59147c72d9667515f010b94e90f40f5f089d0
Willem Sleegers <w.sleegers@me.com>
2023-10-21 20:53:14 +0200

    Misc

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import { createRoot } from "react-dom/client"
 import { App } from "./App"
 
 import { FluentProvider, webLightTheme } from "@fluentui/react-components"
@@ -7,13 +6,17 @@ import { FluentProvider, webLightTheme } from "@fluentui/react-components"
 window.Office.onReady((info) => {
   console.log(`Office.js is now ready in ${info.host} on ${info.platform}`)
 
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
+  if (info.host) {
+    createRoot(document.getElementById("root")!).render(
       <FluentProvider theme={webLightTheme}>
         {/* <StrictMode> */}
         <App host={info.host} />
         {/* </StrictMode> */}
       </FluentProvider>
-    </React.StrictMode>
-  )
+    )
+  } else {
+    createRoot(document.getElementById("root")!).render(
+      <div>Loaded outside of Microsoft Word.</div>
+    )
+  }
 })
