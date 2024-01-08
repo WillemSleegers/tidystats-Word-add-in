1dadfd7e6471402a524ff4f590a4468057b9e3f1
willemsleegers <w.sleegers@me.com>
2024-01-09 00:38:58 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,13 +8,26 @@ window.Office.onReady((info) => {
   console.log(`Office.js is now ready in ${info.host} on ${info.platform}`)
 
   if (info.host) {
-    createRoot(document.getElementById("root")!).render(
-      <FluentProvider theme={webLightTheme}>
-        {/* <StrictMode> */}
-        <App host={info.host} />
-        {/* </StrictMode> */}
-      </FluentProvider>
-    )
+    if (
+      navigator.userAgent.indexOf("Trident") !== -1 ||
+      navigator.userAgent.indexOf("Edge") !== -1
+    ) {
+      createRoot(document.getElementById("root")!).render(
+        <div id="tridentmessage">
+          This add-in will not run in your version of Office. Please upgrade
+          either to perpetual Office 2021 (or later) or to a Microsoft 365
+          account.
+        </div>
+      )
+    } else {
+      createRoot(document.getElementById("root")!).render(
+        <FluentProvider theme={webLightTheme}>
+          {/* <StrictMode> */}
+          <App host={info.host} />
+          {/* </StrictMode> */}
+        </FluentProvider>
+      )
+    }
   } else {
     createRoot(document.getElementById("root")!).render(
       <div>Loaded outside of Microsoft Word.</div>
